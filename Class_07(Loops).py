679369b2a7f22d5d34f833667827b8365f940da6
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-02-21 23:25:34 +0500

    Update

--- Class_07(Loops).py
+++ Class_07(Loops).py
@@ -1,16 +1,39 @@
-#Some are the examples of loops in Python
-#Example 1: Basic for loop
-numbers = [1, 2, 3, 4, 5]
-for num in numbers:
-    print(num * 2)
+#Loops in Python
+#Loops have the two types in the Python programming language.
+#1. For loop
+#2. While loop
+#For loop is used to iterate over a sequence (list, tuple, string) or other iterable objects.
+#While loop is used to execute a block of code repeatedly until a condition is met.
+#For loop is used to iterate over a sequence (list, tuple, string) or other iterable objects.
 
-#Example 2: While loop
-count = 0
+#While loop
+#While loop is used to execute a block of code repeatedly until a condition is met.
+
+a = 10
+while a >= 0:
+    a -= 1
+    print(a)
+
+count = 4
 while count < 5:
-    print(f"Count is {count}")
-    count += 1
+print("Count is 5")    
 
-#Example 3: For loop with range
-for i in range(10):
-    if i % 2 == 0:
-        print(i)
+
+
+# #Some are the examples of loops in Python
+# #Example 1: Basic for loop
+# numbers = [1, 2, 3, 4, 5]
+# for num in numbers:
+#     print(num * 2)
+
+# #Example 2: While loop
+# count = 0
+# while count < 5:
+#     print(f"Count is {count}")
+#     count += 1
+
+# #Example 3: For loop with range
+# for i in range(10):
+#     if i % 2 == 0:
+#         print(i)
+
